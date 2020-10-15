7b3ec74e21e62ee0c3ff28878a7feef4cb77d250
Taco-chii <72862259+Taco-chii@users.noreply.github.com>
2020-10-16 00:06:13 +0800

    Add files via upload

---
+++ Act1_3Numbers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // these are my variables 	o(>ω<)o
+    int a, b, c;
+
+    //°˖✧◝(⁰▿⁰)◜✧˖° this is where well take the inputs of the variables
+    cout << "Enter 1st Number: ";
+    cin >> a;
+    cout << "Enter 2nd Number: ";
+    cin >> b;
+    cout << "Enter 3rd Number: ";
+    cin >> c;
+
+    //ଘ(੭ˊ꒳​ˋ)੭✧ now were gonna fin out which is the largest number of the inputs
+    if(a>b){
+        if (a>c){
+            cout << a << " is the largest number.";
+        }
+        else{
+            cout << c << " is the largest number.";
+        }
+    }
+    else if (b>a){
+        if (b>c){
+            cout << b << " is the largest number.";
+        }
+        else{
+            cout << c << " is the largest number.";
+        }
+    }
+    return 0;
+}
+
+/*	٩(◕‿◕｡)۶ oh hello there, welcome to my code (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+
+    This code is made by
+
+    Roanne Zoe M. Cayanan
+
+    please do not steal my code (´｡• ᵕ •｡`) ♡
+
+    Thank you	♡ ～('▽^人)
+*/
